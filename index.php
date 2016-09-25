c02b9301e5780057295de437380d4823492e4de6
Darshan Joshi <joshids.work@gmail.com>
2016-09-25 14:11:57 +0200

    layout created. Section 3-11

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>My Online Shope</title>
+<link rel="stylesheet" href="styles/style.css" media="all" />
+</head>
+<body>
+    <div class="main_wrapper">
+        <div class="header_wrapper">
+            <img id="logo" src="images/logo.gif" />
+            <img id="banner" src="images/ad_banner.gif" />
+        </div>        
+            <div class="menubar">This is menubar</div>
+        <div class="content_wrapper">
+        <div id="sidebar">This is sidebar</div>
+        <div id="content_area">This is content area</div>
+        </div>
+        <div id="footer">This is the footer</div>
+    </div>
+</body>
+</html?
