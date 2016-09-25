ee9b2f8df38601cc15da46c3e98147810de4353f
Darshan Joshi <joshids.work@gmail.com>
2016-09-25 14:26:40 +0200

    menu created. Section 3-12

--- index.php
+++ index.php
@@ -10,7 +10,16 @@
             <img id="logo" src="images/logo.gif" />
             <img id="banner" src="images/ad_banner.gif" />
         </div>        
-            <div class="menubar">This is menubar</div>
+        <div class="menubar">
+            <ul id="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">All products</a></li>
+                <li><a href="">My account</a></li>
+                <li><a href="">Sign up</a></li>
+                <li><a href="">Shopping cart</a></li>
+                <li><a href="">Contact us</a></li>                
+            </ul>
+        </div>
         <div class="content_wrapper">
         <div id="sidebar">This is sidebar</div>
         <div id="content_area">This is content area</div>
