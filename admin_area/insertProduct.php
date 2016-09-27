3afe1b36946d7535bab71f5bc60f611617e5a8c2
Darshan Joshi <joshids.work@gmail.com>
2016-09-27 06:26:27 +0200

    intermediate commit.

--- admin_area/insertProduct.php
+++ admin_area/insertProduct.php
@@ -21,7 +21,7 @@
             padding-left: 0px;
     </style>
     <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-  <script>tinymce.init({ selector:'textarea' });</script>
+    <script>tinymce.init({ selector:'textarea' });</script>
 </head>
 <body>
     <form action="insertProduct.php" method="post" enctype="multipart/form-data">
@@ -33,12 +33,12 @@
             </tr>
             <tr>
                 <td class="form-label">Title</td>
-                <td><input type="text" name="product_title" class="form-control"></td>
+                <td><input type="text" name="product_title" class="form-control" size="60" required></td>
             </tr>
             <tr>
                 <td class="form-label">Category</td>
                 <td>
-                    <select name="product_category" class="form-control">
+                    <select name="product_category" class="form-control" required>
                         <option value="">Select a categoy</option>
                         <?php
                             getCategoryOptions(); 
@@ -49,7 +49,7 @@
             <tr>
                 <td class="form-label">Brand</td>
                 <td>                    
-                    <select name="product_brand" class="form-control">
+                    <select name="product_brand" class="form-control" required>
                         <option value="">Select a brand</option>
                         <?php
                             getBrandOptions(); 
@@ -59,12 +59,12 @@
             </tr>
             <tr>
                 <td class="form-label">Image</td>
-                <td><input type="file" name="product_image" class="form-control"></td>
+                <td><input type="file" name="product_image" class="form-control" required></td>
             </tr>
             <tr>
                 <td class="form-label">Price</td>
                 <td>
-                    <input type="text" name="product_price" class="form-control">
+                    <input type="text" name="product_price" class="form-control" required>
                 </td>
             </tr>
             <tr>
@@ -75,7 +75,7 @@
             </tr>
             <tr>
                 <td class="form-label">Keywords</td>
-                <td><input type="text" name="product_keywords" class="form-control"></td>
+                <td><input type="text" name="product_keywords" class="form-control" size="50" required></td>
             </tr>
             <tr align="center">                
                 <td colspan="2"><input type="submit" name="insert_product" value="Insert Product Now" /></td>
@@ -87,6 +87,20 @@
 
 <?php
 
+if(isset($_POST['insert_product'])){
+    //get text data from the fields
+    $title = $_POST['product_title'];
+    $categoryId = $_POST['product_category'];
+    $brandId = $_POST['product_brand'];
+    $price = $_POST['product_price'];
+    $desc = $_POST['product_description'];
+    $keywords  = $_POST['product_keywords'];
 
+    //getting the image from the field
+    $productImage = $_FILES['product_image']['name'];
+    $productImage_tmp = $_FILES['product_image']['tmp_name'];
+
+    $sql = "";
+}
 
 ?>
